7a1f45f6cd19bec35cc335c5e3fa7381a19449e0
user <gudapati.geetha@gmail.com>
2022-10-19 11:24:56 +0530

    test

---
+++ src/programs/printnumbers.java
@@ -0,0 +1,20 @@
+package programs;
+
+import java.util.Scanner;
+
+public class printnumbers {
+
+	public static  void main(String[] args) {
+		
+		int n=25063;
+		
+		System.out.println("the numbers" +n);
+		
+		
+		
+		
+		
+		
+	}
+
+}
